d0e3070dfe6ef179b3be4e80a364527940539eb3
Shamsher-Desai <84436441+Shamsher-Desai@users.noreply.github.com>
2021-05-19 14:36:06 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+text = input("Enter A Encrypt Text To Show Decrypt Message\n")
+shift = 3
+
+def encrypt(text,shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        
+        if (char.islower()):
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+ 
+        else:
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+ 
+    return result
+ 
+
+print("Actual  : " + text)
+#print("Shift : " + str(shift))
+print("Encoded: " + encrypt(text,shift))
